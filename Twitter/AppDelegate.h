18fd567bf210826d06bd9c1b7d02e770012045b8
Doupan Guo <doupan@yahoo-inc.com>
2015-02-05 07:52:29 -0800

    Initial Commit

---
+++ Twitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Twitter
+//
+//  Created by Doupan Guo on 2/5/15.
+//  Copyright (c) 2015 Doupan Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Twitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Twitter
+//
+//  Created by Doupan Guo on 2/5/15.
+//  Copyright (c) 2015 Doupan Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
